fdc8379dc8b88f2e27db48f6a794f881e88bc557
Rolland <gisuser.earth@gmail.com>
2018-03-04 18:20:28 -0500

    fix some bugs

--- src/app/components/usertable/usertable.component.ts
+++ src/app/components/usertable/usertable.component.ts
@@ -1,11 +1,11 @@
+import { User } from './../../models/user.model';
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
-
-import { User } from '../../models/user.model';
 import { UserDataSource } from '../../models/user.datasource';
 import { MatMenu, MatMenuTrigger } from '@angular/material';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Component({
   selector: 'app-usertable',
@@ -16,11 +16,35 @@ export class UsertableComponent implements OnInit {
   dataSource = new UserDataSource(this.userService);
   displayedColumns = ['name', 'email', 'phone', 'company'];
 
+  uniqueValues = new Set();
+
   constructor(private userService: UserService) { }
   ngOnInit() {}
 
+  // set column value filter
   onColumnValueFilterChange(selectedValues) {
     console.log(selectedValues.length + ' = ' + selectedValues );
+  }
+
+  populateUniqueValues(colName) {
+    this.uniqueValues.clear();
+    const users = this.dataSource.connect();
+    users.subscribe((result: User[]) => {
+      result.forEach(aUser => {
+        if (colName === 'name') {
+          this.uniqueValues.add(aUser.name);
+        } else if (colName === 'email') {
+          this.uniqueValues.add(aUser.email);
+        } else if (colName === 'company') {
+          this.uniqueValues.add(aUser.company.name);
+        }
+        console.log(aUser.name);
+      });
+      console.log(result.length + ' - ' + this.uniqueValues.size);
+
+    }), (
+      console.log('error')
+    );
   }
 }
 
